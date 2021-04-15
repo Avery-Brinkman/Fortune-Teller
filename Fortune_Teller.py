f3a55d107bede99c42b5e6648ceb1d8b6e8e9c8e
Avery Brinkman <brinkmae@mail.uc.edu>
2021-04-15 19:27:15 -0400

    Fun little thing :)

--- Fortune_Teller.py
+++ Fortune_Teller.py
@@ -1,1 +1,131 @@
+cardDeck = {
+1 : ['The Crimson King', 'Uncovering dangerous secrets', 'Knowledge at great cost', 'A powerful and dangerous man'],
+2 : ['The Scarlett Queen', 'Dangerous bargains', 'Victory only at great cost', 'A powerful and dangerous woman'],
+3 : ['The Cardinal Knight', 'Violence or War', 'End reached through bloody conflict', 'Angry or violent young man'],
+4 : ['The Azure King', 'Time of learning', 'Knowledge will prove invaluable', 'A sage or scholar'],
+5 : ['The Cobalt Queen', ' Something forgotten will return', 'Forgetting a wrong or slight will cause it to happen again', 'Someone you never thought you’d see again'],
+6 : ['The Cyan Knight', 'Facing the consequences of ignorance', 'What you don’t know will hurt you', 'An unexpected enemy'],
+7 : ['The Viridian King', 'Consequences', 'Someone you wronged will have their revenge', 'Someone with a personal grudge'],
+8 : ['The Emerald Queen', 'Conspiracies', 'A plot against you will play out', 'A jealous woman'],
+9 : ['The Jade Knight', 'Rivalries', 'If you don’t take them seriously enough, a rival will best you', 'A rival'],
+10 : ['The Obsidian King', 'A warned event will come to pass', 'Success or failure will depend on how warnings were heeded', 'A male practitioner of dark magic'],
+11 : ['The Onyx Queen', 'A change of forture', 'A reversal, either good or bad', 'A woman whose first impression matters'],
+12 : ['The Black Knight', 'A grudge', 'You will make an enduring enemy', 'A soldier from a defeated army'],
+13 : ['The Bone King', 'News of a death', 'Final endings', 'A dying man'],
+14 : ['The Frost Queen', 'Loss of someone powerful’s favor', 'Fall from grace', 'A widow'],
+15 : ['The Pale Knight', 'Return of a trouble thought over', 'Recurring trouble', 'Someone thought to be dead'],
+16 : ['The Herald of Sorrows', 'Bad news', 'Things will end poorly for someone close', 'A tragic youth'],
+17 : ['The King’s Assassin', 'A betrayal by someone powerful', 'Make a powerful enemy', 'A traitor in a position of advantage'],
+18 : ['The Queen’s Thief', 'Underhanded dealings', 'Loss of something valuable', 'A charming rogue'],
+19 : ['The Conqueror', 'A shift in personal power', 'Dramatic gain or loss of power', 'An ambitious man'],
+20 : ['The High Priest', 'A test of faith (Religious or otherwise)', 'Success or loss depends on the aid of others', 'Someone of a devout or religious order'],
+21 : ['The Witch', 'A string of bad luck', 'Something will have to be set right before luck can turn', 'A female practitioner of dark magic'],
+22 : ['The Gypsy', 'Scams and false flattery', 'Trust the wrong people, and they will take you for a ride', 'Someone young and charming'],
+23 : ['The Jester', 'Loss or respect or jokes at your expense', 'You will be made a laughingstock', 'Someone whose reputation has suffered'],
+24 : ['The Virgin', 'Unfamiliar circumstances', 'You will be forced to navigate a challenge unprepared', 'An innocent'],
+25 : ['The Whore', 'A financial loss', 'May fall prey to excessive spending', 'An ambitious woman'],
+26 : ['The Alchemist', 'Physical or Spiritual transition', 'Irreversible personal change', 'A practitioner of strange sciences'],
+27 : ['The Physician', 'Aid from another', 'Slow healing physical, mental, or spiritual wounds', 'Someone you already put faith in (Parent, teacher, etc.)'],
+28 : ['The Occultist', 'Painful Knowledge', 'Something wicked comes', 'Someone who should not be here'],
+29 : ['The Hunter', 'Gains through long pursuit', 'The chase will yield it’s prizes', 'Someone on a quest or mission'],
+30 : ['The Bleeding Man', 'Pain', 'No matter the outcome, the path will be suffering', 'A victim or persecution or torture'],
+31 : ['The Scarred Man', 'Changed by suffering', 'Painful events will have lasting ramifications', 'Someone who carries the physical or mental marks of a great wrong'],
+32 : ['The Judge', 'Decisions', 'Positive outcomes only achieved if choices are made with reason, not passion', 'Older man in a position of authority'],
+33 : ['The Jury', 'Judgement', 'Outcome determined by others', 'A group of strangers'],
+34 : ['The Headsman', 'The Rule of Law', 'Law upheld', 'A lawman'],
+35 : ['The Traveler', 'A journey', 'A change of locations', 'Someone from far away'],
+36 : ['The Twins', 'A powerful bond', 'Live or die for someone', 'Someone with a powerful spiritual bond to the drawer or two people bonded to each other (identical twins, soul mates, etc.)'],
+37 : ['Sacrifice', 'A willing loss', 'Results for others worth personal cost', 'A man willingly suffering'],
+38 : ['Birth', 'Gain that will change everything', 'Someone or something will come into your life that will drastically alter it', 'A pregnant woman'],
+39 : ['Death', 'Complete transition', 'Irreversible change', 'Someone in mourning'],
+40 : ['The Wedding', 'Unification and bonds', 'Formation of loves, friendships, or strong alliances', 'A new and close friend or ally'],
+41 : ['The Funeral', 'An ending or conclusion', 'Bittersweet parting', 'Someone who has come to you because of a mutual loss'],
+42 : ['The Tournament', 'An exciting challenge', 'A shot at glory', 'A friendly rival'],
+43 : ['Feast', 'Joy and festicity', 'Impressive but temporary gains', 'A wealthy and generous man'],
+44 : ['Famine', 'Times of need', 'Crushing but temporary losses', 'A miser'],
+45 : ['Reincarnation', 'New beginnings', 'The start of a new life', 'Someone who has undergone a recent transition'],
+46 : ['The Ravens', 'Aftermath of tragedy', 'Profiting from loss', 'Opportunist or Profiteer'],
+47 : ['The White Wolf', 'Spiritual awakening', 'A personal epiphany', 'A spiritualist'],
+48 : ['The Black Dog', 'Misfortune', 'No way to get out unscathed', 'Someone cursed'],
+49 : ['The Red Stallion', 'An adventure', 'A new and exciting experience', 'A bold adventurer'],
+50 : ['The Silver Stag', 'A chase or pursuit', 'Chance at an elusive prize', 'An enigmatic stranger'],
+51 : ['The Bull', 'Rapid progress', 'Ends achieved through brute force and determination', 'A laborer or someone of similar origins'],
+52 : ['The Goat', 'Overcoming obstacles', 'Resilience and tenacity will win the day', 'Someone from harsh circumstances'],
+53 : ['The Cat ', ' You will overhear something', 'Solutions will come through subtlety and discretion', 'Someone frequently disregarded or underestimated'],
+54 : ['The Rat ', ' Subterfuge and underhanded dealings', 'The most clever will win', 'A spy or thief'],
+55 : ['The Two-Headed Serpent', 'Flattery and false promises', 'Grave consequences if you trust the wrong person', 'A charming liar'],
+56 : ['The Spider ', ' Beware of traps and snares', 'Failure unless you spot all the complications', 'Someone of few words and complex machinations'],
+57 : ['The Mermaid ', ' Seduction and risky affairs', 'heartbreak and tragic endings', 'a heartsick woman'],
+58 : ['The Manticore ', ' Close alliances and co-conspirators', 'your misdeeds will bind you to your partners in crime', 'A gang or party'],
+59 : ['The Viper ', "Meddling in places you don't belong", 'there week be a cost for getting involved in matters that are not your place', 'a dangerous loner'],
+60 : ['The Leviathan ', ' A natural or unavoidable disaster', 'outcome determined by circumstances beyond your control', 'Someone linked to the Void'],
+61 : ['The Scorpion ', ' Someone will try and ruin you', 'You will be struck at at every turn', 'Someone with a petty grudge'],
+62 : ['The Wasps ', ' Mass hysteria and mob rule', 'outcome will be decided by the crowd', 'a group or mob'],
+63 : ['The Bear', 'Guarding that which you love', 'Someone or something you care about will rely on you to aid them', 'Protective older person'],
+64 : ['The Sword ', ' Strength', 'Your strength will determine the outcome', 'A knight or military officer'],
+65 : ['The Spear ', ' Protection', 'Your ability to protect someone or something will determine the outcome ', ' A guard or soldier'],
+66 : ['The Axe ', ' You will be the agent of justice', ' your judgement or actions will determine if justice is served', 'a vigilante'],
+67 : ['The Hammer', 'Force', 'Outcome will be determined by martial prowess', 'A ruffian or barbarian'],
+68 : ['The Knife', 'Guile', 'Outcome will be determined by cunning', 'A courtier or diplomat'],
+69 : ['The Rose', 'Winning fame or favor', 'You success will earn you reputation and acclaim', 'A young and beautiful woman'],
+70 : ['The Broken Tower ', ' Calamity', 'Failure of Endeavors', 'Someone who has suffered a recent failure.'],
+71 : ['The Bridge', 'Joining together of two unlikely things', 'Unexpected alliances', 'A foe turned friend'],
+72 : ['The Gallows Tree', 'Punishment', 'Making amends for your actions or words', 'A criminal'],
+73 : ['The Poisoned Cup', 'A gift, reward, or treasure will bring misfortune', 'Even an apparent success will reap sorrow', 'Someone who will try to ply you with false generosity'],
+74 : ['The Chalice', 'Joy and reward', 'Your endeavors will yield success and great gains', 'Someone famous'],
+75 : ['The Sea', 'Journey over water', 'Cross a great physical distance', 'A fickle woman'],
+76 : ['The Lightning Struck Ship', 'A difficult journey', 'A life-or-death struggle', 'A sudden adversary'],
+77 : ['The Desert', 'Loneliness and isolation', 'A struggle that must be faced alone', 'A wanderer or hermit'],
+78 : ['The Mountain', 'Test of physical or mental endurance', 'Toughness will win the day', 'An unusually large person'],
+79 : ['The Forest', 'Confounding factors and unclear paths', 'A confusing journey, easy to lose sight of the purpose', 'A woodsman or ranger'],
+80 : ['The Path', 'Clarity of purpose', 'Intent and purpose will be clear or obnvious', 'A tracker or guide'],
+81 : ['The Stairway', 'Opportunities', 'Great things if you proceed carefully, a terrible slip and fall if you do not', 'An architect or builder'],
+82 : ['The Rising Sun', 'New beginnings and limitless potential', 'Your actions in the immediate future will have long-lasting benefits or consequences', 'A young child'],
+83 : ['The Sun at Zenith', 'Hard work will yield great benefits', 'You will sow and reap in equal measure', 'Someone respected and established in their trade'],
+84 : ['The Setting Sun', 'Rest and peaceful conclusions', 'The ending of a job or similar', 'A retired person'],
+85 : ['The New Moon', 'The unknown', 'Unexpected event will change everything', 'A blind man'],
+86 : ['The Crescent Moon', 'Half-truths and incomplete predictions', 'Unsatisfactory conclusions and unfinished business', 'a restless youth'],
+87 : ['The Full Moon', 'True predictions', 'An unfolding of prophecy or fortunes', 'A seer'],
+88 : ['Wind', 'Constant changes and reversals', 'No results, good or bad, will endure for long', 'A moody or unstable person'],
+89 : ['Snow', 'Confusion and obfuscation', 'You will remain in ignorance or confusion', 'A mute'],
+90 : ['The Star ', ' A quest or journey', 'the start of a new mission', 'a mentor or guide'],
+91 : ['The Demon', 'Punishment', 'All sins punished', 'A torturer'],
+92 : ['The Devil', 'Bargains and deals', 'An arrangement twisted against you', 'A drafter of contracts'],
+93 : ['The Fey', 'Mysteries', 'Inconclusive or strange endings', 'A mysterious or unknown person'],
+94 : ['The Dragon', 'A life-changing challenge', 'Death or glory', 'A powerful adversary'],
+95 : ['Dreams', 'Unlikely adventures or encounters with the uncanny', 'The ending will be a far cry from where you began', 'Someone fae-touched'],
+96 : ['Nightmares', 'Things long-feared will come to pass', 'Things will appear far worse before they get better', 'A madman'],
+97 : ['Time', 'Facing the inevitable', 'Change that you are powerless to fight', 'Someone important from your past'],
+98 : ['The Treasure Hoard', 'The prize at the end of the challenge', 'Great reward after great challenge or suffering', 'Someone you will have to fight for'],
+99 : ['DM’s Choice', 'DM’s Choice', 'DM’s Choice', 'DM’s Choice'],
+100 : ['Something that should not be. Make up a card that isn’t in the deck.','Something that should not be. Make up a card that isn’t in the deck.', 'Something that should not be. Make up a card that isn’t in the deck.', 'Something that should not be. Make up a card that isn’t in the deck.']
+}
+# Card deck and mechanics from https://www.reddit.com/r/DnD/comments/37gded/i_made_an_npc_card_reader/
+
+import random
+import time
+
+random.seed(time.time())
+
+t = ['       Immediate Future      ', '         Near Future         ', 'Conclusion of Cycle of Events']
+
+for i in t:
+    print(31 * '=')
+    print('|', end = '')
+    print(i, end = '|\n')
+    print(31 * '=')
+
+    choices = [int(random.random() * 100) + 1 for _ in range(3)]
+
+    print('Cards: ', end = '\n  ')
+    for c in choices:
+        print(cardDeck[c][0], end = '\n  ')
+
+    print()
+
+    print('Fortune: ')
+    print(' ' + cardDeck[choices[0]][1] + ', ' + cardDeck[choices[1]][2] + ', ' + cardDeck[choices[2]][3])
+
+    print()
+
 
